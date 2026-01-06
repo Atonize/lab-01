a1c0f7ddb2b6315a1617f409dba0bb617246ab0f
Atonize <aamalia@ualberta.ca>
2026-01-06 15:18:38 -0700

    participation exercise

---
+++ PetShop/app/src/main/java/com/example/petshop/Mood.java
@@ -0,0 +1,24 @@
+package com.example.petshop;
+import java.util.Date;
+
+public abstract class Mood {
+    protected Date date;
+
+    public Mood() {
+        date = new Date();
+    }
+
+    public Mood(Date date) {
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public abstract String status();
+}
